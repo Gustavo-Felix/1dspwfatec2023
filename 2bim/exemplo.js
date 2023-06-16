524a460f0e1a314fbcffe082cc47f81cd460d7c4
Gustavo_Félix <80945334+Gustavo-Felix@users.noreply.github.com>
2023-06-16 16:04:20 -0300

    Add files via upload

---
+++ 2bim/exemplo.js
@@ -0,0 +1,29 @@
+function receber_nome(dados) {
+    let valor = prompt(`Informe seu ${dados}`)
+    return valor;
+
+}
+
+let funcao_variavel = function (dado) {
+    
+}
+
+
+class exemplo {
+    constructor() {
+
+    }
+    acao(dado) {
+        
+
+    }
+}
+
+let coisa = new Exemplo();
+
+const funcao_arrow = (dado) => {
+    let valor = prompt(`Informe seu ${dado}`)
+    return valor;
+
+
+}
